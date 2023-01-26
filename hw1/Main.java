d7f208316216590ef16c166a4f590eb06773ecc4
Zhukov Nikita <nypno@mail.ru>
2023-01-26 21:19:09 +0300

    added check

--- hw1/Main.java
+++ hw1/Main.java
@@ -9,16 +9,28 @@ public class Main {
     public static void main(String[] args) {
         int n = getNumberByUser("Введите коэффициент для треугольного числа: ");
         int tr = getTriangularNum(n);
-        System.out.println(tr);
+        if(tr < 1){
+            System.out.println("Введите натуральное число!");
+        }else {
+            System.out.printf("%d-ое треугольное число = %d",n,tr);
+        }
     }
 
 
     public static int getNumberByUser(String text) {
         System.out.print(text);
-        return input.nextInt();
+        String s = input.nextLine();
+        boolean isNumeric = s.chars().allMatch( Character::isDigit );
+        if (!isNumeric){
+            return -1;
+        }
+        return Integer.parseInt(s);
     }
 
-    public static int getTriangularNum(int index){
+    public static int getTriangularNum(int index) {
+        if (index < 1){
+            return index;
+        }
         if (index == 1) return 1;
         return getTriangularNum(index - 1) + index;
     }
---
+++ hw2/Main.java
@@ -0,0 +1,2 @@
+package homework.hw2;public class Main {
+}
