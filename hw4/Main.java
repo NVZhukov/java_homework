8187622f9c0ef1c923a2bf0a8cc62a882f064a16
Zhukov Nikita <nypno@mail.ru>
2023-02-04 20:56:03 +0300

    make method for wavelogarithm and get info from user

--- hw4/Main.java
+++ hw4/Main.java
@@ -2,20 +2,51 @@ package homework.hw4;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
+        int[] data = getInfo();
 
-        int[][] lab = new int[10][10];
-        int[] start = new int[]{1, 2};
+        int[][] lab = new int[data[0]][data[1]];
 
+        int[][] res = waveLogarithm(lab,data[2] - 1,data[3] - 1,data[4] - 1,data[5] - 1);
+        printTab(res);
+    }
+
+    public static int[] getInfo(){
+        int[] info = new int[6];
+        try{
+            System.out.println("Введите количество строк на карте: ");
+            info[0] = sc.nextInt();
+            System.out.println("Введите количество столбцов на карте: ");
+            info[1] = sc.nextInt();
+            System.out.println("Введите координату Х начальной точки: ");
+            info[2] = sc.nextInt();
+            System.out.println("Введите координату У начальной точки: ");
+            info[3] = sc.nextInt();
+            System.out.println("Введите координату Х конечной точки: ");
+            info[4] = sc.nextInt();
+            System.out.println("Введите координату У конечной точки: ");
+            info[5] = sc.nextInt();
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Введите корректные данные!");
+        }
+        return info;
+    }
+
+    public static int[][] waveLogarithm(int[][] lab, int x, int y, int fx, int fy){
+        int[] start = new int[]{x, y};
         Queue<int[]> queueOfNext = new LinkedList<>();
         queueOfNext.add(start);
 
         int[] dotStart = queueOfNext.element();
         lab[dotStart[0]][dotStart[1]] = 1;
 
-        while (lab[8][8] == 0) {
+        while (lab[fx][fy] == 0) {
 
             int[] dot = queueOfNext.element();
 
@@ -25,13 +56,13 @@ public class Main {
                 queueOfNext.add(arr1);
             }
 
-            if ((dot[1] + 1) < 10 && lab[dot[0]][dot[1] + 1] == 0) {
+            if ((dot[1] + 1) < lab.length && lab[dot[0]][dot[1] + 1] == 0) {
                 lab[dot[0]][dot[1] + 1] = lab[dot[0]][dot[1]] + 1;
                 int[] arr2 = new int[]{dot[0], dot[1] + 1};
                 queueOfNext.add(arr2);
             }
 
-            if ((dot[0] + 1) < 10 && lab[dot[0] + 1][dot[1]] == 0) {
+            if ((dot[0] + 1) < lab.length && lab[dot[0] + 1][dot[1]] == 0) {
                 lab[dot[0] + 1][dot[1]] = lab[dot[0]][dot[1]] + 1;
                 int[] arr3 = new int[]{dot[0] + 1, dot[1]};
                 queueOfNext.add(arr3);
@@ -42,12 +73,10 @@ public class Main {
                 int[] arr5 = new int[]{dot[0], dot[1] - 1};
                 queueOfNext.add(arr5);
             }
-            printTab(lab);
             queueOfNext.remove();
         }
-        printTab(lab);
+        return lab;
     }
-
 
     public static void printTab(int[][] tab) {
         for (int i = 0; i < tab.length; i++) {
